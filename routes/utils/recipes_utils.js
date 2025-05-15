d8202bf694954612828d4c29e2961eb31bde562e
ShoganA <abdalla.shogan@gmail.com>
2025-05-15 14:01:40 +0300

    setup continue

--- routes/utils/recipes_utils.js
+++ routes/utils/recipes_utils.js
@@ -18,6 +18,27 @@ async function getRecipeInformation(recipe_id) {
     });
 }
 
+
+
+//  from lab7 TODO: check for the correct ruoting path
+
+/**
+ * Get recipes list from spooncular response and extract the relevant recipe data for preview
+ * @param {*} recipes_info 
+ */
+async function getRecipeInformationFROMLAB(recipe_id) {
+  //https://api.spoonacular.com/recipes/complexSearch
+  return await axios.get(`${api_domain}/recipes/${recipe_id}/information`, {
+    params: {
+      includeNutrition: false,
+      // apiKey: "e43e55e61c3a45028f3147ef60ea76be"
+      apiKey: process.env.spooncular_apiKey
+    }
+  });
+}
+
+//  from lab7
+
 async function getRecipeDetails(recipe_id) {
     let recipe_info = await getRecipeInformation(recipe_id);
     let { id, title, readyInMinutes, image, aggregateLikes, vegan, vegetarian, glutenFree } = recipe_info.data;
@@ -36,6 +57,55 @@ async function getRecipeDetails(recipe_id) {
 }
 
 
+
+
+
+//  from lab7
+
+function extractPreviewRecipeDetails(recipes_info) {
+  return recipes_info.map((recipe_info) => {
+    //check the data type so it can work with diffrent types of data
+    let data = recipe_info;
+    if (recipe_info.data) {
+      data = recipe_info.data;
+    }
+    const {
+      id,
+      title,
+      readyInMinutes,
+      image,
+      aggregateLikes,
+      vegan,
+      vegetarian,
+      glutenFree,
+    } = data;
+    return {
+      id: id,
+      title: title,
+      image: image,
+      readyInMinutes: readyInMinutes,
+      popularity: aggregateLikes,
+      vegan: vegan,
+      vegetarian: vegetarian,
+      glutenFree: glutenFree
+    }
+  })
+}
+
+async function getRecipesPreview(recipes_ids_list) {
+  let promises = [];
+  recipes_ids_list.map((id) => {
+    promises.push(getRecipeInformation(id));
+  });
+  let info_res = await Promise.all(promises);
+  info_res.map((recp) => { console.log(recp.data) });
+  //   console.log(info_res);
+  return extractPreviewRecipeDetails(info_res);
+}
+
+getRecipesPreview(["663559", "642582", "655705", "652100"]);
+
+//  from lab7
 
 exports.getRecipeDetails = getRecipeDetails;
 
